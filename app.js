619862c87561b0e77ff06a5ce2714ed56dae20aa
Hotpause <ashutoshagarwal1006@gmail.com>
2024-07-24 12:55:06 +0530

    Add passport functionality

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 const express = require("express");
+const passport = require("./config/passport");
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+const pool = require("./config/database");
+const { createUserTable } = require("./models/user");
---
+++ config/passport.js
@@ -0,0 +1,40 @@
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const { findUserByUsername } = require("../models/users");
+const passport = require("passport");
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await findUserByUsername(username);
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await findUserByUsername(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+module.exports = passport;
