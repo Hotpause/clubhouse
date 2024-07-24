15269239dd814bdb48f8203583a151e891b548de
Hotpause <ashutoshagarwal1006@gmail.com>
2024-07-24 11:34:59 +0530

    Add db schema

---
+++ models/messages.js
@@ -0,0 +1,32 @@
+import { pool } from "../config/database";
+
+const createMessageTable = async () => {
+  const query = `CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        title VARCHAR(100) NOT NULL,
+        text TEXT NOT NULL,
+        timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        user_id INTEGER REFERENCES users(id)
+    );
+`;
+  await pool.query(query);
+};
+
+const addMessage = async (title, text, userId) => {
+  const query = `
+  INSERT INTO messages (title, text, user_id) 
+  VALUES ($1,$2,$3)
+  RETURNING *;
+  `;
+  const values = [title, text, userId];
+  const result = await pool.query(query, values);
+  return result.rows[0];
+};
+
+const getAllMessages = async () => {
+  const query = `SELECT * FROM messages`;
+  const result = await pool.query(query);
+  return result.rows;
+};
+
+module.exports = { createMessageTable, addMessage, getAllMessages };
---
+++ models/users.js
@@ -0,0 +1,33 @@
+import { pool } from "../config/database";
+const bcrypt = require("bcrypt");
+
+const createUserTable = async () => {
+  const query = `
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    membership_status BOOLEAN DEFAULT FALSE,
+    is_admin BOOLEAN DEFAULT FALSE);
+    `;
+
+  await pool.query(query);
+};
+
+const addUser = async (firstName, lastName, username, password) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const query = `INSERT INTO users(firstName, lastName, username, password) VALUES ($1,$2,$3,$4) RETURNING *;`;
+  const values = [firstName, lastName, username, hashedPassword];
+  const result = await pool.query(query, values);
+  return result.rows[0];
+};
+
+const findUserByUsername = async (username) => {
+  const query = `SELECT * FROM users WHERE username = $1`;
+  const result = await pool.query(query, [username]);
+  return result.rows[0];
+};
+
+module.exports = { createUserTable, addUser, findUserByUsername };
